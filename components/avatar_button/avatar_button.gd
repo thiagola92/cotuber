4b089bbd16e5451b1d5ba11438dcd67aa97a07c0
thiagola92 <thiagola92@gmail.com>
2024-12-08 15:35:20 -0300

    Backup

--- components/avatar_button/avatar_button.gd
+++ components/avatar_button/avatar_button.gd
@@ -1,11 +1,24 @@
 extends Button
 
 
-signal image_dropped(image: Image)
+signal new_image(image: Image)
+
+@onready var _file_dialog := $FileDialog
 
 
 func _ready() -> void:
+	_file_dialog.hide()
 	get_window().files_dropped.connect(_on_window_files_dropped)
+
+
+func _set_image(file: String) -> void:
+	var image := Image.new()
+	var error := image.load(file)
+	
+	if error:
+		return ErrorPopup.show_message("AVATAR_BUTTON_ERROR_FORMAT")
+	
+	new_image.emit(image)
 
 
 func _on_window_files_dropped(files: PackedStringArray) -> void:
@@ -16,10 +29,12 @@ func _on_window_files_dropped(files: PackedStringArray) -> void:
 	if len(files) != 1:
 		return ErrorPopup.show_message("AVATAR_BUTTON_ERROR_DROPPED")
 	
-	var image := Image.new()
-	var error := image.load(files[0])
-	
-	if error:
-		return ErrorPopup.show_message("AVATAR_BUTTON_ERROR_FORMAT")
-	
-	image_dropped.emit(image)
+	_set_image(files[0])
+
+
+func _on_pressed() -> void:
+	_file_dialog.popup_centered()
+
+
+func _on_file_dialog_file_selected(path: String) -> void:
+	_set_image(path)
