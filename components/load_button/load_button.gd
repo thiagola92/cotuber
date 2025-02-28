1488a67e18b8ed6b6ca9e57b2d457dcafa4801de
thiagola92 <thiagola92@gmail.com>
2025-02-28 14:02:06 -0300

    Add drop zip to load button

--- components/load_button/load_button.gd
+++ components/load_button/load_button.gd
@@ -12,6 +12,8 @@ signal character_loaded(character: CharacterData)
 func _ready() -> void:
 	if OS.get_name() == "Web":
 		_load_input.init()
+	
+	get_window().files_dropped.connect(_on_window_files_dropped)
 
 
 func _on_pressed() -> void:
@@ -117,6 +119,17 @@ func _load(path: String) -> void:
 		character.states.append(state)
 	
 	character_loaded.emit(character)
+
+
+func _on_window_files_dropped(files: PackedStringArray) -> void:
+	# Check if is dropping at this Control.
+	if not Rect2(global_position, size).has_point(get_global_mouse_position()):
+		return
+	
+	if len(files) != 1:
+		return ErrorPopup.show_message("LOAD_BUTTON_ERROR_DROPPED")
+	
+	_load(files[0])
 
 
 func _on_file_dialog_file_selected(path: String) -> void:
