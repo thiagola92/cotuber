8f8778c565d64f823def067a140ce46320b1a1fb
thiagola92 <thiagola92@gmail.com>
2025-05-10 05:53:25 -0300

    Minimum of Discord communication implemented

--- components/voice_servers/discord/voice_server_discord.gd
+++ components/voice_servers/discord/voice_server_discord.gd
@@ -10,24 +10,38 @@ var _call: DiscordppCall
 
 
 func _ready() -> void:
+	id = str(client.GetCurrentUser().Id())
 	_call = client.StartCall(lobby_id)
 	
-	_call.SetParticipantChangedCallback(_on_participant_changed)
 	_call.SetSpeakingStatusChangedCallback(_on_speaking_status_changed)
+	_call.SetParticipantChangedCallback(_on_participant_changed)
+
+
+func _process(_delta: float) -> void:
+	Discordpp.RunCallbacks()
+
+
+func _on_speaking_status_changed(user_id: int, is_playing_sound: bool) -> void:
+	if is_playing_sound:
+		speaking[str(user_id)] = true
+		voice_started.emit(str(user_id))
+	else:
+		speaking[str(user_id)] = false
+		voice_stopped.emit(str(user_id))
 
 
 func _on_participant_changed(user_id: int, added: bool) -> void:
-	print("user: %s\nadded: %s" % [user_id, added])
-
-
-func _on_speaking_status_changed(user_id: int, isPlayingSound: bool) -> void:
-	print("user: %s\nspeaking: %s" % [user_id, isPlayingSound])
+	if str(user_id) == id:
+		return
+	
+	if added:
+		create_user(str(user_id), CharacterData.new())
+		voice_connected.emit(str(user_id))
+	else:
+		delete_user(str(user_id))
+		voice_disconnected.emit(str(user_id))
 
 
 func _on_tree_exiting() -> void:
 	client.Disconnect()
 	client = null
-
-
-func _process(_delta: float) -> void:
-	Discordpp.RunCallbacks()
